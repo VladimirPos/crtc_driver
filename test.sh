2b946e22feffe367848f25e8c923ee0b5ee5d767
VladimirPos <v.pososhnikov@yandex.ru>
2019-03-03 07:51:01 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,22 +4,22 @@ insmod crtc.ko
 echo 'showing module info'
 modinfo crtc.ko
 echo crtc: `hwclock -f /dev/rtc1`
-echo rtc `hwclock`
+echo rtc: `hwclock`
 echo 'sleep 10s'
 sleep 10s
 echo crtc: `hwclock -f /dev/rtc1`
-echo rtc `hwclock`
+echo rtc: `hwclock`
 echo 'fast mode'
 echo 1 > /proc/crtc
 cat /proc/crtc
 echo "set value to crtc"
 `hwclock -f /dev/rtc1 --set --date "2000/01/01 00:00:00.0"`
 echo crtc: `hwclock -f /dev/rtc1`
-echo rtc `hwclock`
+echo rtc: `hwclock`
 echo 'sleep 10s'
 sleep 10s
 echo crtc: `hwclock -f /dev/rtc1`
-echo rtc `hwclock`
+echo rtc: `hwclock`
 echo 'slow mode'
 echo 0 > /proc/crtc
 cat /proc/crtc
@@ -27,10 +27,10 @@ echo "set value to crtc"
 `hwclock -f /dev/rtc1 --set --date "2000/01/01 00:00:00.0"`
 sleep 1
 echo crtc: `hwclock -f /dev/rtc1`
-echo rtc `hwclock`
+echo rtc: `hwclock`
 echo 'sleep 10s'
 sleep 10s
 echo crtc: `hwclock -f /dev/rtc1`
-echo rtc `hwclock`
+echo rtc: `hwclock`
 echo 'removing module'
 rmmod crtc.ko
